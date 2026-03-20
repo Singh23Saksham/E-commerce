84fd4224f55f361da0be41577bb8c443a02e689d
Singh23Saksham <150496204+Singh23Saksham@users.noreply.github.com>
2026-03-20 15:34:08 +0530

    Change button link href to relative path
    Updated the href for the button link to use a relative path.

--- cart.js
+++ cart.js
@@ -1,4 +1,4 @@
-console.clear();
+// console.clear();
 
 if(document.cookie.indexOf(',counter=')>=0)
 {
@@ -80,7 +80,7 @@ let buttonTag = document.createElement('button')
 buttonDiv.appendChild(buttonTag)
 
 let buttonLink = document.createElement('a')
-buttonLink.href = '/orderPlaced.html?'
+buttonLink.href = './orderPlaced.html?'
 buttonTag.appendChild(buttonLink)
 
 buttonText = document.createTextNode('Place Order')
